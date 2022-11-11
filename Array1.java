a530c9836cbf6ad37c4c8dbbd80aa3d2d0345f22
Alvaro <a-alvaro.diaz.casano@iespablopicasso.es>
2022-11-11 14:32:02 +0100

    Imprimir números por pantalla arrray.java

--- Array1.java
+++ Array1.java
@@ -1,16 +1,29 @@
-public class Array {
+import java.util.Scanner;
+
+public class Array1 {
 
   public static void main(String[] args) {
     
     final int LONGITUDNUMEROS = 20;
     final int NUMEROMINIMO = 1;
     final int NUMEROMAXIMO= 10;
+   
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("NÚMEROS");
     
     int[] numeros = new int[LONGITUDNUMEROS]; 
     for (int i = 0; i < LONGITUDNUMEROS; i++) {
       numeros[i] = (int)(Math.random() * (NUMEROMAXIMO - NUMEROMINIMO + 1) + NUMEROMINIMO );
-      
     }
-
+    
+    System.out.println("ARRAY ORIGINAL ");
+    for (int i = 0; i < LONGITUDNUMEROS; i++) {
+      System.out.print(numeros[i] + " ");
+    }
+    System.out.println();
+    
+    System.out.println("Fin del programa");
   }
+
 }
